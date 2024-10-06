28f90156d60cb959767f23cca9a588ee14eee705
Tushar Ranjan <107484915+tushar110302@users.noreply.github.com>
2024-10-06 15:22:11 +0530

    Create codes-file.c

---
+++ codes-file.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+struct stud{
+    int age[10];
+    char *p;
+    float f;
+};
+int a;
+int main() {
+    // Write C code here
+    // printf("Try programiz.pro\n");
+    // int a[3][4] = {{1,2,3,4},{5,6,7,8}};
+    
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<4;j++){
+    //         printf("%d",a[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // int a[3][4] = {1,2,3,4,5,6,7,8,9,67};
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<4;j++){
+    //         printf("%d ",a[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n%u\n",a);
+    // printf("\n%u\n",*(a+1)+1);
+    // printf("\n%u\n",a+2);
+    
+    // struct stud s1;
+    // printf("%d\n",sizeof(int*));
+    // printf("%d\n",sizeof(float*));
+    // printf("%d\n",sizeof(char*));
+    // printf("%d\n",sizeof(double*));
+    // printf("%d\n",sizeof(s1));
+    
+    // struct stud a,*p;
+    // p=&a;
+    // printf("%u\n",&a);
+    // printf("%u\n",&a+1);
+    // printf("%u\n",p);
+    // printf("%u\n",(p+1)-1);
+    
+    // printf("%d",a);
+    
+    return 0;
+}
